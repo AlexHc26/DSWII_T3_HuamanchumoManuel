b7cf9ec192b5bbf310c65a4eb2dc1aa53ba86513
AlexHc26 <mahc2601@gmail.com>
2024-07-02 13:45:18 -0500

    proyecto creado

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_HuamanchumoManuel/DswiiT3HuamanchumoManuelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_HuamanchumoManuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3HuamanchumoManuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3HuamanchumoManuelApplication.class, args);
+	}
+
+}
